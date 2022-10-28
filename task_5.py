9c9dfd5740938d1059097cc4238084888ffcced4
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-10-28 16:55:53 +0200

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,23 @@
 def cipher(text, shift):
-    return text
+    result = ""
+    for character in text:
+        if character == " ":
+            result = result + character
+        elif character.isupper():
+            character = ord(character)
+            if character >= 90:
+                character = character - 26
+            character = character + shift
+            character = chr(character)
+            result = result + character
+        else:
+            character = ord(character)
+            if character >= 122:
+                character = character - 26
+            character = character + shift
+            character = chr(character)
+            result = result + character
+
+    return result
 
 
-def decipher(text, shift):
-    return text
