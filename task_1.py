9141423cf25f97e21fe087e5129a59844a571087
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-10-31 15:25:26 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,15 @@
 def remove_parentheses(text):
-    return text
+    result = ""
+    is_inside = False
+    i = 0
+    while i < len(text):
+        if text[i] == "(":
+            is_inside = True
+        elif text[i] == ")":
+            is_inside = False
+            i = i + 1
+        elif not is_inside:
+            result = result + text[i]
+
+        i = i + 1
+    return result
